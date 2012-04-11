795da67c4cf4dc22197887d9567655cac6218c21
newguy <gdzack@gmail.com>
2012-04-11 14:25:51 +1000

    Completed a listview demo with gallery dialog component.

--- androiddemo/src/com/google/tsang/AndroiddemoActivity.java
+++ androiddemo/src/com/google/tsang/AndroiddemoActivity.java
@@ -1,16 +1,42 @@
 package com.google.tsang;
 
-import android.app.Activity;
-import android.app.FragmentManager;
+import android.app.DialogFragment;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
-public class AndroiddemoActivity extends Activity {
+/**
+ * Main entry
+ * 
+ * @author Yi Zeng
+ *
+ */
+public class AndroiddemoActivity extends ListActivity 
+    implements OnItemClickListener {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        FragmentManager fm = getFragmentManager();
+        ListView listView = getListView();
+        // Uses the string-array in strings.xml to generate contents for the list
+        ListAdapter adapter = ArrayAdapter.createFromResource(
+                this, R.array.list_content, R.layout.list_control);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(this);
     }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position,
+            long id) {
+        final String selectedItem = (String) parent.getItemAtPosition(position);
+        final DialogFragment galleryDialog = 
+                GalleryDialog.getInstance(R.string.dialog_title, selectedItem);
+        galleryDialog.show(getFragmentManager(), "Gallery Dialog");
+    }
+    
 }
---
+++ androiddemo/src/com/google/tsang/GalleryDialog.java
@@ -0,0 +1,191 @@
+package com.google.tsang;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+/**
+ * A dialog fragment designed for gallery display
+ * 
+ * @author Yi Zeng
+ *
+ */
+public class GalleryDialog extends DialogFragment {
+    
+    private static final String DIALOG_TITLE = "title";
+    private static final String DIALOG_CONTENT = "dialogContent";
+
+    private GalleryDialog() {
+        // Protected from initializing from constructor
+    }
+    
+    public static GalleryDialog getInstance(int title, String dialogContent) {
+        GalleryDialog gDialog = new GalleryDialog();
+        final Bundle args = new Bundle();
+        args.putInt(DIALOG_TITLE, title);
+        args.putString(DIALOG_CONTENT, dialogContent);
+        gDialog.setArguments(args);
+        gDialog.setCancelable(true);
+        return gDialog;
+    }
+    
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View v = (View)inflater.inflate(R.layout.gallery_fragment, container, false);
+        // Get images from resource
+        initImages();
+        
+        Gallery gallery = (Gallery) v.findViewById(R.id.gallery);
+        String dialogContent = getArguments().getString(DIALOG_CONTENT);
+        gallery.setAdapter(new ImageAdapter(getActivity(), getImages(dialogContent)));
+        
+        // Set the title for the dialog
+        int title = getArguments().getInt(DIALOG_TITLE);
+        String titleString = getResources().getString(title);
+        getDialog().setTitle(titleString);
+        
+        Button btnBack = (Button) v.findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {   
+                getDialog().dismiss();
+            }
+        });
+        
+        return v;
+    }
+    
+    private Integer[] imageNSW = {
+            R.drawable.sydney, 
+            R.drawable.blue_mountain,
+            R.drawable.bondi,
+            R.drawable.south_coast,
+            R.drawable.silverton_nsw
+    };
+    private Integer[] imageVIC = {
+            R.drawable.gibson_steps, 
+            R.drawable.greate_ocean_road,
+            R.drawable.stmarys_church, 
+            R.drawable.twelve_apostles
+    };
+    private Integer[] imageSA = {
+            R.drawable.sa1, 
+            R.drawable.sa2};
+    private Integer[] imageQueensland = {
+            R.drawable.queensland1,
+            R.drawable.queensland2,
+            R.drawable.queensland3
+    };
+    private Integer[] imageWA = {
+            R.drawable.wa,
+            R.drawable.wa_rock
+    };
+    private Integer[] imageNT = {
+            R.drawable.na1,
+            R.drawable.na2,
+            R.drawable.na3
+    };
+    private Integer[] imageTasmania = {
+            R.drawable.tasmania1,
+            R.drawable.tasmania2,
+            R.drawable.tasmania3,
+    };
+    private Integer[] imageACT = {
+            R.drawable.canberra1,
+            R.drawable.canberra2
+    };
+    
+    final Map<String, Integer[]> imageMap = new HashMap<String, Integer[]>();
+    
+    private Integer[] getImages(String dialogContent) {
+        return imageMap.get(dialogContent);
+    }
+    
+    private void initImages() {
+        for (String s : getResources().getStringArray(R.array.list_content)) {
+            if ("New South Wales".equals(s)) {
+                imageMap.put(s, imageNSW);
+            }
+            else if ("Victoria".equals(s)) {
+                imageMap.put(s, imageVIC);
+            }
+            else if ("South Australia".equals(s)) {
+                imageMap.put(s, imageSA);
+            }
+            else if ("Queensland".equals(s)) {
+                imageMap.put(s, imageQueensland);
+            }
+            else if ("Western Australia".equals(s)) {
+                imageMap.put(s, imageWA);
+            }
+            else if ("Australian Capital Territory".equals(s)) {
+                imageMap.put(s, imageACT);
+            }
+            else if ("Northern Territory".equals(s)) {
+                imageMap.put(s, imageNT);
+            }
+            else if ("Tasmania".equals(s)) {
+                imageMap.put(s, imageTasmania);
+            }
+        }
+    }
+    
+    // customized ImageAdapter for displaying images of the given state
+    private class ImageAdapter extends BaseAdapter {
+        int mGalleryItemBackground;
+        private Context mContext;
+        
+        private Integer[] mImageIds;
+        
+        public ImageAdapter(Context c, Integer[] imageIds) {
+            mContext = c;
+            mImageIds = imageIds;
+            TypedArray attr = mContext.obtainStyledAttributes(R.styleable.MyGallery);
+            mGalleryItemBackground = attr.getResourceId(
+                    R.styleable.MyGallery_android_galleryItemBackground, 0);
+            attr.recycle();
+        }
+        
+        @Override
+        public int getCount() {
+            return mImageIds.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return position;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(mContext);
+
+            imageView.setImageResource(mImageIds[position]);
+            imageView.setLayoutParams(new Gallery.LayoutParams(360, 240));
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            imageView.setBackgroundResource(mGalleryItemBackground);
+            
+            return imageView;
+
+        }
+        
+    }
+}
